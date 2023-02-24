c6b4de7052be0ea18a2c972da0e405720d056b6f
Je Dela Cruz <93860350+jedelacruz@users.noreply.github.com>
2023-02-24 09:01:42 +0800

    Create Madlib.java

---
+++ Madlib.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+public class Madlib {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String adjective,verb,adjective2,noun2,verb2,adjective3,noun3,adjective4,adjective5,noun4,noun5;
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("MADLIBS");
+		System.out.println("");
+		
+		System.out.print("Adjective:"); 
+		adjective = sc.nextLine();
+		
+		System.out.print("Verb:"); 
+		verb = sc.nextLine();
+		
+		System.out.print("Adjective:"); 
+		adjective2 = sc.nextLine();
+		
+		System.out.print("Noun:"); 
+		noun2 = sc.nextLine();
+		
+		System.out.print("Verb:"); 
+		verb2 = sc.nextLine();
+		
+		System.out.print("Adjective:"); 
+		adjective3 = sc.nextLine();
+
+		System.out.print("Noun:"); 
+		noun3 = sc.nextLine();
+		
+		System.out.print("Adjective:"); 
+		adjective4 = sc.nextLine();
+		
+		System.out.print("Adjective:"); 
+		adjective5 = sc.nextLine();
+		
+		System.out.print("Noun:"); 
+		noun4 = sc.nextLine();
+		
+		System.out.print("Noun:"); 
+		noun5 = sc.nextLine();
+		
+		System.out.println("");
+		
+		System.out.print("Our School cafeteria has really " + adjective + " food." + " Just \nthinking about it makes my stomach " + verb + ". The \nspaghetti is " + adjective2 + " and tastes like " + noun2 + ". One \nday, I swear one of my meatballs started to " + verb2 + " ! The \nturkey tacos are totally " + adjective3 + " and look kind of like old \n" + noun3 + " . My friend Dana actually likes the meatloaf, even \nthough it's " + adjective4 + " and " + adjective5 + " 'mystery meatloaf' and think it's really made out of " + noun4 + ". My \ndad said he'd make my lunches, but the first day, he made me\na sandwich out of " + noun5 + " and peanut butter! I think I'd \nrather take my chances with the cafeteria!");
+		
+		
+		
+		
+	}
+
+}
